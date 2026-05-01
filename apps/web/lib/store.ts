55d138969f2928163ee467c6dfb255009f62bfb6
Harmen <harmendark@gmail.com>
2026-05-01 13:05:00 +1200

    update store for workflow editor state

--- apps/web/lib/store.ts
+++ apps/web/lib/store.ts
@@ -3,14 +3,26 @@ import { Workflow } from '@flowcore/shared/types'
 
 interface WorkflowState {
   workflows: Workflow[]
+  currentWorkflow: Workflow | null
+  steps: Step[]
   setWorkflows: (workflows: Workflow[]) => void
   addWorkflow: (workflow: Workflow) => void
+  setCurrentWorkflow: (workflow: Workflow | null) => void
+  setSteps: (steps: Step[]) => void
+  addStep: (step: Step) => void
 }
 
 export const useWorkflowStore = create<WorkflowState>((set) => ({
   workflows: [],
+  currentWorkflow: null,
+  steps: [],
   setWorkflows: (workflows) => set({ workflows }),
   addWorkflow: (workflow) => set((state) => ({ 
     workflows: [workflow, ...state.workflows] 
+  })),
+  setCurrentWorkflow: (workflow) => set({ currentWorkflow: workflow }),
+  setSteps: (steps) => set({ steps }),
+  addStep: (step) => set((state) => ({ 
+    steps: [...state.steps, step].sort((a, b) => a.order - b.order) 
   })),
 }))
